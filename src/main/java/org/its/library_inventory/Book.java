1109c9cd252fb6f94fb0756f2aef912b96d2be25
Juan Carlos <143283368+Chispa8@users.noreply.github.com>
2024-07-26 16:06:44 +0100

    Update Book.java

--- src/main/java/org/its/library_inventory/Book.java
+++ src/main/java/org/its/library_inventory/Book.java
@@ -14,6 +14,7 @@ public class Book {
     private String title;
     private String author;
     private String isbn;
+    @Temporal(TermporalType.DATE)
     private Date publishedDate;
     private Double price;
 
